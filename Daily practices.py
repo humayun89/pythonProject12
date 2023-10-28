8763bfdfa067b085a532723613292f7c2da95be0
HumayunRChy <humayun.chowdhury@metropolia.fi>
2023-10-29 01:19:24 +0300

    Initial commit

---
+++ Daily practices.py
@@ -0,0 +1,12 @@
+# Use of format :
+name = "Humayun"
+age = 34
+print ("My name is {} and i am {} years old".format(name,age))
+
+# while loop and format:
+i = 3000
+while i<10000:
+    i+=150
+    print("i is :{}".format(i))
+
+
