dbdd90324bab2b5bae4027882fa5e9dbdbe050d4
HumayunRChy <humayun.chowdhury@metropolia.fi>
2023-10-29 03:01:23 +0300

    new testing

--- Daily practices.py
+++ Daily practices.py
@@ -8,5 +8,9 @@ i = 3000
 while i<10000:
     i+=150
     print("i is :{}".format(i))
+# Using range function in for loop :
+for i in range (0,10):
+    i+=1
+    print("i value is {}" .format(i))
 
 
